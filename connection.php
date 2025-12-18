bdf95ea49ff499606ed02a9a43f5bc25dba57e16
Emmanuel Ace <emmanueleacelam@gmail.com>
2025-12-18 11:20:36 +0300

    Update connection.php to use config.php settings for database connection

--- connection.php
+++ connection.php
@@ -1,11 +1,22 @@
 <?php
-//code for connecting to the database
+// Include configuration file
+require_once 'config.php';
 
-        $host ="localhost";
-        $db_user ="root";
-        $db_password ="Pronunciation";
-        $db_name = "registration";
-        $conn ="";
-        
-        $conn = mysqli_connect($host, $db_user, $db_password, $db_name);
+// Create connection using config.php settings
+$connection = new mysqli(
+    CONFIG['db_host'],
+    CONFIG['db_user'],
+    CONFIG['db_password'],
+    CONFIG['db_name']
+);
+
+// Check connection
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+}
+
+// Set character set to utf8
+$connection->set_charset("utf8");
+
+// Return the connection object
 ?>
